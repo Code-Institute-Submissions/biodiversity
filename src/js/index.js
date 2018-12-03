49315f5ce30c3622bca284d1c9c288468c7f7f76
PeteWillmott <bastior@yahoo.co.uk>
2018-12-03 21:02:50 +0100

    corrected errors in stacked chart

--- src/js/index.js
+++ src/js/index.js
@@ -157,162 +157,96 @@ function makeGraphs(error, akparksData, parksizeData) {
         }
         );
 
-        var speciesNumByCatAlgae = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Algae') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+        function speciesNumByCat(cat) {
+              return pname_dim.group().reduce(
+                  function (p, v) {
+                      p.total++;
+                      if(v.Category == cat) {
+                          p.match++;
+                      }
+                      return p;
 
-        var speciesNumByCatAmphibean = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Amphibian') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-       var speciesNumByCatBird = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Bird') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+                  },
+                  function (p, v) {
+                      p.total--;
+                      if(v.Category == cat) {
+                          p.match--;
+                      }
+                      return p;
+                  },
+                  function () {
+                      return {total: 0, match: 0};
+                  }
+              );
+          }
 
-       var speciesNumByCatCrustacean = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Crab/Lobster/Shrimp') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+          var speciesNumByCatAlgae = speciesNumByCat("Algae");
+          var speciesNumByCatAmphibean = speciesNumByCat("Amphibian");
+          var speciesNumByCatBird = speciesNumByCat("Bird");
+          var speciesNumByCatCrustacean = speciesNumByCat("Crab/Lobster/Shrimp");
+          var speciesNumByCatFish = speciesNumByCat("Fish");
+          var speciesNumByCatFungi = speciesNumByCat("Fungi");
+          var speciesNumByCatInsect = speciesNumByCat("Insect");
+          var speciesNumByCatInvertebrate = speciesNumByCat("Invertabrate");
+          var speciesNumByCatMammal = speciesNumByCat("Mammal");
+          var speciesNumByCatNonvascularPlant = speciesNumByCat("Nonvascular Plant");
+          var speciesNumByCatReptile = speciesNumByCat("Reptile");
+          var speciesNumByCatSlug = speciesNumByCat("Slug/Snail");
+          var speciesNumByCatSpider = speciesNumByCat("Spider/Scorpion");
+          var speciesNumByCatVascularPlant = speciesNumByCat("Vascular Plant") ;
 
-        var speciesNumByCatFish = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Fish') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+          dc.pieChart("#num-species")
+            .height(600)
+            .radius(300)
+            .transitionDuration(1500)
+            .dimension(category_dim)
+            .group(category_group)
+            .legend(dc.legend().x(40).y(0).gap(5))
+            .externalLabels(50)
+            .externalRadiusPadding(50);
 
-        var speciesNumByCatFungi = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Fungi') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+          dc.pieChart("#park-size")
+            .height(600)
+            .radius(300)
+            .transitionDuration(1500)
+            .dimension(pname_dim)
+            .group(park_size_group)
+            .legend(dc.legend().x(40).y(0).gap(5))
+            .externalRadiusPadding(50)
+            .valueAccessor(function (d) {
+            return d.value.average;
+            });
 
-           var speciesNumByCatInsect = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Insect') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+          var stackedChart = dc.barChart("#speciescat-stack");
+          stackedChart
+              .width(1000)
+              .height(1500)
+              .dimension(pname_dim)
+              .group(speciesNumByCatAlgae, "Algae")
+              .stack(speciesNumByCatAmphibean, "Amphibian")
+              .stack(speciesNumByCatBird, "Bird")
+              .stack(speciesNumByCatCrustacean, "Crustacean")
+              .stack(speciesNumByCatFish, "Fish")
+              .stack(speciesNumByCatFungi, "Fungi")
+              .stack(speciesNumByCatInsect, "Insect")
+              .stack(speciesNumByCatInvertebrate, "Invertabrate")
+              .stack(speciesNumByCatMammal, "Mammal")
+              .stack(speciesNumByCatNonvascularPlant, "Nonvascular Plant")
+              .stack(speciesNumByCatReptile, "Reptile")
+              .stack(speciesNumByCatSlug, "Slug")
+              .stack(speciesNumByCatSpider, "Spider")
+              .stack(speciesNumByCatVascularPlant, "Vascular Plant")
+              .valueAccessor(function(d) {
+                  return d.value.match;
+              })
+              .x(d3.scale.ordinal())
+              .xUnits(dc.units.ordinal)
+              .legend(dc.legend().x(40).y(0).itemHeight(15).gap(5));
 
-           var speciesNumByCatInvertebrate = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Invertebrate') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
+          stackedChart.margins().left = 300;
+          stackedChart.margins().right = 10;
+          stackedChart.margins().top = 10;
+          stackedChart.margins().bottom = 300;
 
-           var speciesNumByCatMammal = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Mammal') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-
-           var speciesNumByCatNonvascularPlant = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Nonvascular Plant') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-
-           var speciesNumByCatReptile = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Reptile') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-
-           var speciesNumByCatSlug = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Slug/Snail') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-
-           var speciesNumByCatSpider = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Spider/Scorpion') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-
-           var speciesNumByCatVascularPlant = pname_dim.group().reduceCount(function (d) {
-               if (d.Category === 'Vascular Plant') {
-                   return +d.SpeciesName ;
-               } else {
-                   return 0;
-               }
-           });
-
-      dc.pieChart("#num-species")
-        .height(1000)
-        .radius(500)
-        .transitionDuration(1500)
-        .dimension(category_dim)
-        .group(category_group);
-
-      dc.pieChart("#park-size")
-        .height(1000)
-        .radius(500)
-        .transitionDuration(1500)
-        .dimension(category_dim)
-        .dimension(pname_dim)
-        .group(park_size_group)
-        .valueAccessor(function (d) {
-        return d.value.average;
-        });
-
-    var stackedChart = dc.barChart("#speciescat-stack");
-       stackedChart 
-       .width(1000)
-       .height(1000)
-       .dimension(pname_dim)
-       .group(speciesNumByCatAlgae, "Algae")
-       .stack(speciesNumByCatAmphibean, "Amphibian")
-       .stack(speciesNumByCatBird, "Bird")
-       .stack(speciesNumByCatCrustacean, "Crustacean")
-       .stack(speciesNumByCatFish, "Fish")
-       .stack(speciesNumByCatFungi, "Fungi")
-       .stack(speciesNumByCatInsect, "Insect")
-       .stack(speciesNumByCatInvertebrate, "Invertabrate")
-       .stack(speciesNumByCatMammal, "Mammal")
-       .stack(speciesNumByCatNonvascularPlant, "Nonvascular Plant")
-       .stack(speciesNumByCatReptile, "Reptile")
-       .stack(speciesNumByCatSlug, "Slug")
-       .stack(speciesNumByCatSpider, "Spider")
-       .stack(speciesNumByCatVascularPlant, "Vascular Plant")
-       .x(d3.scale.ordinal())
-       .xUnits(dc.units.ordinal)
-       .legend(dc.legend().x(470).y(0).itemHeight(15).gap(5));
-
-       stackedChart.margins().left = 100;
-       stackedChart.margins().right = 10;
-       stackedChart.margins().top = 10;
-       stackedChart.margins().bottom = 300;
-
-    dc.renderAll();
+      dc.renderAll();
 }
