6fa9d6f168e77354ac482710f545842c4f6ac369
PeteWillmott <bastior@yahoo.co.uk>
2018-12-28 12:48:37 +0100

    Tidy label styling

--- src/js/index.js
+++ src/js/index.js
@@ -92,12 +92,14 @@ function makeGraphs(error, akparksData) {
             .transitionDuration(1500)
             .dimension(pname_dim)
             .group(park_size_group)
-            .legend(dc.legend().x(40).y(0).gap(5))
+            .legend(dc.legend().x(0).y(150).gap(5))
             .externalRadiusPadding(50)
+            .externalLabels(20)
             .colors(d3.scale.ordinal().range([ '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f']))
             .valueAccessor(function (d) {
             return d.value.average;
-            });
+            })
+            .minAngleForLabel(0);
 
         dc.pieChart("#num-species")
             .height(600)
@@ -105,7 +107,7 @@ function makeGraphs(error, akparksData) {
             .transitionDuration(1500)
             .dimension(category_dim)
             .group(category_group)
-            .legend(dc.legend().x(40).y(0).gap(5))
+            .legend(dc.legend().x(0).y(0).gap(5))
             .externalLabels(100)
             .externalRadiusPadding(50);
             //.colors(function(d){ return colorScale(d.Category); });
